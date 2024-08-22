62a8d2c85fb85eb07c6c05cc66b46f685fd9f9cf
Your Name <you@example.com>
2024-08-22 13:49:30 +0530

    first commit

---
+++ backend/app.js
@@ -0,0 +1,247 @@
+import 'dotenv/config';
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+import ejs from "ejs";
+import {dirname} from "path" ;
+import {fileURLToPath} from "url";
+import session from "express-session";
+import bcrypt from "bcrypt";
+import passport from "passport";
+import { Strategy } from "passport-local";
+import path from "path";
+
+const app = new express();
+const port = process.env.PORT || 3000;
+const saltRounds=10;
+
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: true,
+// Use true in production with HTTPS
+}));
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+const __dirname= dirname(fileURLToPath(import.meta.url));
+console.log(__dirname);
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, '../frontend/public')));
+
+// Set the view engine to EJS
+app.set('view engine', 'ejs');
+const db = new pg.Client({
+   user:"postgres",
+   host:"localhost",
+   database:"world",
+   password:"191023",
+   port:process.env.db||5432
+});
+db.connect();
+//To get all task 
+async function getTaskList(){
+    const task_detail= await db.query("SELECT * FROM gettask($1,$2,$3,$4,$5,$6,$7,$8,$9,$10)"[user.id,user.email,0,NULL,NULL,NULL,NULL,NULL,NULL,0]);
+    return task_detail;
+}
+async function getAllTaskList(req,res){
+    const resultSet= await getTaskList(); 
+    if(resultSet.rowCount){
+     res.render("activity.ejs",{
+        tasksList:resultSet.rows,
+        count:1,
+        userId:req.session.user
+     });
+    }
+    else {
+        res.render("activity.ejs",{
+            count:0,
+            tasksList:'No task added , Add now',
+            userId:req.session.user
+         }); 
+    }
+}
+
+
+app.get("/",(req,res)=>{
+    res.render("index");
+});
+app.get("/performance",(req,res)=>{
+  res.render("performance");
+});
+app.get("/login",(req,res)=>{
+    res.render("login");
+});
+app.get("/signup",(req,res)=>{
+    res.render("signup");
+})
+
+app.get("/logout", (req, res) => {
+    req.logout(function (err) {
+      if (err) {
+        return next(err);
+      }
+      res.redirect("/");
+    });
+  });
+
+app.post('/delete-task', async (req, res) => {
+  if (req.isAuthenticated()) {
+    const {taskId} = req.body; 
+    const parsedTaskIds = taskId.map(id => parseInt(id, 10)).map(id => parseInt(id, 10));
+    const placeholders = parsedTaskIds.map((_, index) => `$${index + 2}`).join(', ');
+    const query =`UPDATE tasks SET deleted = ($1) where task_id IN (${placeholders})`;
+    const values = [1, ...parsedTaskIds];
+    const result = await db.query(query, values);
+    if (result) {
+      res.status(200).send({ message: 'Task deleted successfully' });
+    } else {
+      res.status(404).send({ message: 'Task not found' });
+    }
+  }
+  else{
+    res.redirect("/login");
+  }
+});
+app.get("/dashboard", async (req,res) =>{
+    if (req.isAuthenticated()) {
+      const task_detail=  await db.query("SELECT * FROM gettask($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12)",[req.user.id,req.user.email,0,null,null,null,null,null,null,0,null,null]);
+      const resultSet =  task_detail;
+   
+      if(resultSet.rowCount){
+        res.render("activity",{
+           tasksList:resultSet.rows,
+           count:1,
+           userId:req.user.email
+        });
+       }
+       else {
+           res.render("activity",{
+               count:0,
+               tasksList:'No task added , Add now',
+               userId:req.user.email
+            }); 
+       }
+      } else {
+        res.redirect("/login");
+      }
+      
+   
+});
+
+app.post("/weekTask",(req,res)=>{
+    var result = req.body;
+    const userId = req.user['id'];
+    var queryresult = db.query(" CALL upserttask($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12)",[userId,0,result['activity'],result['description'],result['priority'],0, result['deadline'],result['status'],0,result['impact'],result['reward'],0]);
+    res.redirect('/dashboard');
+    
+});
+
+app.post("/taskEdit",(req,res)=>{
+    var result = req.body;
+    const userId = req.user['id'];
+    var queryresult = db.query(" CALL upserttask($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12)",[userId,result['task_id'],result['activity'],result['description'],result['priority'],0,result['deadline'],result['status'],0,result['impact'],result['reward'],0]);
+    res.redirect('/dashboard');
+    
+});
+
+
+  app.post("/signup", async (req, res) => {
+    const email = req.body.email;
+    const name = req.body.name;
+    const role = req.body.role;
+    const password = req.body.password;
+  
+    try {
+      const checkResult = await db.query("SELECT * FROM users WHERE email = ($1)", [
+        email
+      ]);
+  
+      if (checkResult.rows.length > 0) {
+        req.redirect("/login");
+      } else {
+        bcrypt.hash(password, saltRounds, async (err, hash) => {
+          if (err) {
+            console.error("Error hashing password:", err);
+          } else {
+            const result = await db.query(
+              "INSERT INTO users (email,name,role, password) VALUES ($1, $2,$3,$4) RETURNING *",
+              [email,name,role, hash]
+            );
+            const user = result.rows[0];
+            req.login(user, (err) => {
+              console.log("success");
+              res.redirect("/dashboard");
+            });
+          }
+        });
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+  app.post('/logins',passport.authenticate("local", {
+    successRedirect: "/dashboard",
+    failureRedirect: "/login"
+}));
+
+  passport.use("local",
+    new Strategy({ usernameField: 'email' },async function verify(email, password, cb) {
+      try {
+       
+        const result = await db.query("SELECT * FROM users WHERE email = ($1) ", [
+          email
+        ]);
+       
+        if (result.rows.length > 0) {
+          const user = result.rows[0];
+          const storedHashedPassword = user.password;
+          bcrypt.compare(password, storedHashedPassword, (err, valid) => {
+            if (err) {
+              //Error with password check
+              console.error("Error comparing passwords:", err);
+              return cb(err);
+            } else {
+              if (valid) {
+                //Passed password check
+               
+                return cb(null, user);
+              } else {
+                //Did not pass password check
+                return cb(null, false);
+              }
+            }
+          });
+        } else {
+          return cb("User not found");
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    })
+  );
+  
+  passport.serializeUser((user, cb) => {
+    cb(null, user);
+  });
+  passport.deserializeUser((user, cb) => {
+    cb(null, user);
+  });
+
+
+
+
+app.listen(port, (err,res)=>{
+    if(err){
+        console.log(err)
+    }
+    else{
+        console.log(`Running on ${port}`);
+    }
+})
---
+++ frontend/public/script.js
@@ -0,0 +1,289 @@
+function minimumDate(type,datetoshow){
+    var date = new Date();
+    var month = date.getMonth()+1;
+    var dates = date.getDate();
+    var day = date.getDay();
+    var year = date.getFullYear();
+   
+    
+if (type==='max'){
+    var daysRemaining = 7- day;
+    dates = dates+daysRemaining;
+   if(dates>28){
+
+   if(getnewMonth(month,year) ===28){                
+    dates= dates-28;
+   }
+   else if(getnewMonth(month,year) ===29){
+    dates= dates-29;
+   }
+   else if(getnewMonth(month,year) ===30){
+    dates= dates-29;
+   }
+   if(month===12){
+    month=1;
+    year= year+1;
+   }
+   month=month+1;
+
+}
+
+}
+else if (type==='togetdate'){
+    var date = new Date(datetoshow);
+    var month = date.getMonth()+1;
+    var dates = date.getDate();
+    var day = date.getDay();
+    var year = date.getFullYear();
+}
+if (month<10){
+    month = '0' + month.toString();
+}
+if (dates<10){
+    dates = '0' + dates.toString();
+}
+return (`${year}-${month}-${dates}`);
+}
+function getnewMonth(month,year){
+if (month===1 || month==3 || month===5 || month===7 || month===8 || month===10 || month===12){
+return 31;
+}
+if (month==='04' || month=='06' || month==='09' || month==='11'){
+return 30;
+}
+if(month==='02'){
+if((year%4===0 && year%100!=0 )||year%400){
+return 29;
+}
+else{
+    return 28;
+}
+}
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+var dateInput = document.getElementsByClassName('deadline');
+//console.log(dateInput[1]);
+dateInput[0].setAttribute('min', minimumDate('min',null));
+dateInput[1].setAttribute('min', minimumDate('min',null));
+dateInput[0].setAttribute('max', minimumDate('max',null));
+dateInput[1].setAttribute('max', minimumDate('max',null));
+
+});
+
+const add_icon = Array.from(document.getElementsByClassName("add_icon"));
+add_icon.forEach(function(add_icon,index){
+   add_icon.addEventListener("click", ()=>{
+       const popup= document.getElementsByClassName("pop_up")[0];
+       popup.style.visibility= "visible";
+       document.querySelectorAll('.toblurcontent').forEach((element) => { 
+        if (!element.classList.contains('pop_up')) {
+            element.classList.add('blurred');
+        }
+    });
+       
+});
+
+});
+
+const close_icon = Array.from(document.getElementsByClassName("btn-close"));
+close_icon.forEach(function(close_icon,index){
+    close_icon.addEventListener("click", ()=>{
+        const popup= document.getElementsByClassName("pop_up")[0];
+        const edit_popup= document.getElementsByClassName("edit_pop_up")[0];
+        popup.style.visibility= "hidden";
+        edit_popup.style.visibility= "hidden";
+        document.querySelectorAll('.toblurcontent').forEach((element) => {
+            if (!element.classList.contains('pop_up') || !element.classList.contains('edit_pop_up')) {
+                element.classList.remove('blurred');
+            }
+});
+});
+    });
+
+const edit_icon = Array.from(document.getElementsByClassName("edit_icon"));
+edit_icon.forEach(function(editicon,index){
+     editicon.addEventListener("click", ()=>{
+        const popup= document.getElementsByClassName("edit_pop_up")[0];
+        popup.style.visibility= "visible";
+        document.querySelectorAll('.toblurcontent').forEach((element) => { 
+            if (!element.classList.contains('edit_pop_up')) {
+                element.classList.add('blurred');
+            }
+        });
+});
+    
+    });
+
+document.addEventListener("click",(event)=>{
+    const classRecieved = (event.target.className).split(" ")[0];
+    if(classRecieved==='edit_icon'){
+        const classRequired =(event.target.className).split(" ")[1];
+     const editContent =document.getElementsByClassName(classRequired);
+      const toedit = document.getElementsByClassName("edit_value");
+      console.log(editContent[7].innerHTML);
+      console.log(editContent[3].innerHTML);
+      console.log(editContent[4].innerHTML);
+      //Task name
+       toedit[0].setAttribute('value',editContent[1].innerHTML.trim());
+       //Description
+       toedit[1].setAttribute('value',editContent[3].innerHTML.trim());
+       //Imapct
+       toedit[2].setAttribute('value',editContent[2].innerHTML.trim());
+       //taskid
+       toedit[3].setAttribute('value',editContent[8].innerHTML.trim());
+       //status
+       toedit[4].setAttribute('value',editContent[4].innerHTML.trim());
+       toedit[4].innerHTML=editContent[4].innerHTML.trim();
+       //priority
+       toedit[5].setAttribute('value',editContent[5].innerHTML.trim());
+       toedit[5].innerHTML=editContent[5].innerHTML.trim();
+       //deadline
+       
+        const dateStr = editContent[6].innerHTML.trim();
+        const [day, month, year] = dateStr.split('/');
+       const dateObject = new Date(`${year}-${month}-${day}`);
+       const dates = minimumDate("togetdate",dateObject);
+       toedit[6].value = dates;
+       //reward
+       toedit[7].setAttribute('value',editContent[9].innerHTML.trim());
+
+}
+});
+
+const selectedItems= [];
+const taskid =[];
+const checkboxesClicked = Array.from(document.getElementsByClassName("tick"));
+const add_image = Array.from(document.getElementsByClassName('add_icon'));
+const edit_image =  Array.from(document.getElementsByClassName('edit_icon'));
+// console.log(checkboxesClicked);
+checkboxesClicked.forEach(function(checkbox){
+     checkbox.addEventListener("click", ()=>{
+        console.log(checkbox.classList[1]);
+        const secondClass= checkbox.classList[1];
+        console.log(secondClass);
+        const detail = document.getElementsByClassName(secondClass)[8];
+        let index = selectedItems.indexOf(secondClass)
+        if(index!==-1){
+        selectedItems.splice(index,1);
+        taskid.splice(index,1);
+        }
+        else{
+            selectedItems.push(secondClass);
+            taskid.push(detail.innerHTML)
+        }
+        if (selectedItems.length>0){
+            add_image.forEach(function (add){
+                add.classList.add('disabled');
+            });
+            edit_image.forEach(function (edit){
+                edit.classList.add('disabled');
+            });
+        }
+        else if (selectedItems.length===0) {
+            add_image.forEach(function (add){
+                add.classList.remove('disabled');
+            });
+            edit_image.forEach(function (edit){
+                edit.classList.remove('disabled');
+            });
+        }
+          
+        });
+    });
+        document.getElementsByClassName('delete-icon')[0].addEventListener("click",()=>{
+           if ( confirm("Are you sure you want to delete the tasks ? ")){
+            // Send the task ID to the server using fetch API
+            fetch('/delete-task', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json'
+              },
+              body: JSON.stringify({ taskId: taskid })
+            })
+            .then(response => {
+              if (response.ok) {
+                window.location.reload();
+                console.log('Task deleted successfully');
+              } else {
+                console.error('Failed to delete the task');
+              }
+            })
+            .catch(error => {
+              console.error('Error:', error);
+            });
+        }
+    
+    });   
+ const descriptionLogic = Array.from(document.getElementsByClassName('description'));
+ //console.log(descriptionLogic);
+ descriptionLogic.forEach(function(value){
+    if(value.innerHTML.trim().length===0){
+        value.innerHTML='NA';
+        value.classList.add('na-font');
+      
+    }
+ });
+
+ document.addEventListener('DOMContentLoaded', function () {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+      return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+  });
+
+const statusoftask = Array.from(document.getElementsByClassName('statusoftask'));
+statusoftask.forEach(function(status){
+    console.log(status.innerHTML.trim());
+    if(status.innerHTML.trim()==='To do'){
+        status.classList.add('green');
+    }
+    else if(status.innerHTML.trim()==='In progress'){
+        status.classList.add('orange');
+    }
+    else if(status.innerHTML.trim()==='In review'){
+        status.classList.add('green');
+    }
+});
+
+const priorityoftask = Array.from(document.getElementsByClassName('priorityoftask'));
+priorityoftask.forEach(function(priority){
+    
+    if(priority.innerHTML.trim()==='P1'){
+        priority.classList.add('red');
+    }
+    else if(priority.innerHTML.trim()==='P2'){
+        priority.classList.add('orange');
+    }
+    else if(priority.innerHTML.trim()==='P3'){
+        priority.classList.add('grey');
+    }
+    else if(priority.innerHTML.trim()==='P4'){
+        priority.classList.add('green');
+    }
+});
+document.getElementById('calendar').addEventListener('click', function() {
+    const calendarInput = document.getElementById('calendartoshow');
+    calendarInput.style.display = 'block';
+    calendarInput.focus();
+  });
+document.getElementById('calendartoshow').addEventListener('blur', function() {
+    setTimeout(() => {
+      this.style.display = 'none';
+    }, 200); // Slight delay to ensure the date is selected before hiding
+  });
+
+
+
+// console.log(taskList);
+    // var text =(event.target.className).split(" ")[1]; 
+    // var item1 =document.getElementsByClassName(text)[1];
+    // var item2 = document.getElementsByClassName(text)[2];
+    // var item3= document.getElementsByClassName(text)[3];
+    // item1.classList.toggle('completed');
+    // item2.classList.toggle('completed');
+    // item3.classList.toggle('completed');
+   
+
+   
